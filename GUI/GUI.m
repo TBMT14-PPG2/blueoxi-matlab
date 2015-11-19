9ed998d57696414b0b997be3e4090cb929782035
filgu983 <filgu983@student.liu.se>
2015-11-19 14:17:36 +0100

    GUI change

--- GUI/GUI.m
+++ GUI/GUI.m
@@ -22,7 +22,7 @@ function varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-% Last Modified by GUIDE v2.5 13-Nov-2015 11:25:06
+% Last Modified by GUIDE v2.5 18-Nov-2015 15:22:31
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -82,24 +82,42 @@ function varargout = GUI_OutputFcn(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 
-
 % --- Executes on button press in load.
 function load_Callback(hObject, eventdata, handles)
 % hObject    handle to load (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
-filename=uigetfile('*.wav');
-[Y, Fs]=audioread(filename);
-%[avg_pulse, sig, BPM]=func_pulsecalc(Y, Fs);
-handles.signal=Y;
-handles.Fs=Fs;
-handles.signal_loaded=1;
-guidata(hObject,handles);
-
-% axes(handles.waveform_graph)
-% plot(filtered_signal);
-% handles.waveform_graph=gca;
+if handles.signal_loaded==1
+    set(handles.pulse_value, 'String', '');
+    set(handles.max_pulse_value, 'String', '');
+    set(handles.oxy_value, 'String', '');
+    cla(handles.oxy_graph,'reset')
+    cla(handles.waveform_graph,'reset')
+    guidata(hObject,handles);
+    
+    filename=uigetfile('*.wav');
+    [Y, Fs]=audioread(filename);
+    [avg_pulse, sig, BPM]=func_pulsecalc(Y, Fs);
+    handles.signal=Y;
+    handles.filtered_sig=sig;
+    handles.Fs=Fs;
+    handles.signal_loaded=1;
+    guidata(hObject,handles);
+else
+    filename=uigetfile('*.wav');
+    [Y, Fs]=audioread(filename);
+    [avg_pulse, sig, BPM]=func_pulsecalc(Y, Fs);
+    handles.signal=Y;
+    handles.filtered_sig=sig;
+    handles.Fs=Fs;
+    handles.signal_loaded=1;
+    guidata(hObject,handles);
+    
+    % axes(handles.waveform_graph)
+    % plot(filtered_signal);
+    % handles.waveform_graph=gca;
+end
 
 % --- Executes on button press in play.
 function play_Callback(hObject, eventdata, handles)
@@ -135,7 +153,6 @@ if handles.signal_loaded==0
     PercentageOfMax=0;
     
     %Set graph properties
-    
     axes(handles.oxy_graph)
     handles.oxy_graph = plot(ptp_time,Pulse,'-k','LineWidth',1.5);
     title('Pulse','FontSize',15);
@@ -154,8 +171,6 @@ if handles.signal_loaded==0
     fopen(s);
     tic 
     
-    
-    
     fprintf(s,'a');
     while ishandle(handles.waveform_graph) %Loop when Plot is Active
 
@@ -169,8 +184,6 @@ if handles.signal_loaded==0
         DC(count+1)=DC_val;
         AC(count+1)=AC_val;
         peakmatch=AC_val+DC_val; %max(LP)
-
-
 
         if(~isempty(USB_Data) && isfloat(USB_Data)) %Check if correct data from USB       
             count = count + 1;  %increases for each data received
@@ -206,12 +219,9 @@ if handles.signal_loaded==0
                     %set(handles.oxy_graph,'CurrentAxes')
                     %plot(handles.oxy_graph,'XData',ptp_time,'YData',Pulse);
                     %axes(handles.waveform_graph)
-               
-                    
+                           
                 end
-                
-                
-            
+             
 %             set(handles.oxy_graph,'XData',time,'YData',Pulse);
 %             axis([time(count-k)-timeint time(count-k) Min_waveform Max_waveform]);
 
@@ -219,7 +229,6 @@ if handles.signal_loaded==0
             set(plotGraph,'XData',time,'YData',data);
             axis([0 time(count) Min_waveform Max_waveform]);
             end
-
 
             pause(delay);
         end
@@ -243,16 +252,30 @@ else
     end
 end
 
+% Take out the variables and plot the waveform
+    [avg_pulse, sig, BPM, S, S_avg, ptp_time]=func_pulsecalc(handles.signal, handles.Fs);
+ %   handles.filter_sig = sig;
+    t=[0:1/handles.Fs:(length(sig)-1)/handles.Fs];
+ %        handles.t=[0:1/handles.Fs:10];
+%     sig=1:length(t);
+%     plot(handles.waveform_graph,t(i:i+100),sig(i:i+handles.Fs/10));
+      plot(handles.waveform_graph,t, sig);%(1:length(handles.t)));
+      Ax = findall(0,'type','axes');
+       axis(Ax(3),[0 10 0 4.5e-3]);
+      
+
+ % Call back function displays the current slider value & plots n points
+%     function callbackfn(handles.slider2,eventdata)
+%  m=get(slider_handle, 'Value');
+% set(value_slide,'String',num2str(m))
+% plot(handles.waveform_graph,handles.t, m*sig(1:length(handles.t)));
+
 % for i=1:handles.Fs/10:(length(handles.signal)-handles.Fs/10)
 %     tic;
 %     timer=tic;
-
-    [avg_pulse, sig, BPM, S, S_avg]=func_pulsecalc(handles.signal, handles.Fs);
-%    t=[0:1/handles.Fs:(length(sig)-1)/handles.Fs];
-         t=[0:1/handles.Fs:10];
-%     sig=1:length(t);
-%     plot(handles.waveform_graph,t(i:i+100),sig(i:i+handles.Fs/10));
-     plot(handles.waveform_graph,t, sig(1:length(t)));
+%      handles.waveform_graph = get(handles.slider2,'Value');
+%      m=get(handles.slider_handle, 'Value');
+%      set(value_slide,'String',num2str(m))
      
     %Display the value of the current pulse in tag pulse_value
     set(handles.text20, 'String', 'Avg pulse bpm:');
@@ -272,24 +295,23 @@ end
     %Show BPM and O2 in axes2 with tag oxy_graph
     axes(handles.oxy_graph)
     le=1:length(avg_pulse);
-    le_s=1:length(S);
-    [ax,p1,p2] = plotyy(le/handles.Fs,avg_pulse,le_s/handles.Fs,S,'semilogy','plot');
+    le_ss=1:length(S);
+    le_s=le_ss/handles.Fs;
+    [ax,p1,p2] = plotyy(ptp_time,avg_pulse,le_s,S,'semilogy','plot');
     xlabel(ax(1),'Time [s]') % label x-axis
     ylabel(ax(1),'BPM') % label left y-axis
     ylabel(ax(2),'% O2') % label right y-axis
 %     ylim([30 200])
 %     ylim(ax(2),[60 110]);
-    line(le,avg_pulse,'Color','k')
+%     line(le_s,S,'Color','b')
+%     ax(2).XColor = 'b';
+%     ax(2).YColor = 'b';
+    line(ptp_time,avg_pulse,'Color','k')
     ax(1).XColor = 'k';
     ax(1).YColor = 'k';
-%     line(le_s,S,'Color','r')
-%     ax(2).XColor = 'r';
-%     ax(2).YColor = 'r';
 
 % pause(1/(handles.Fs/10) - toc(timer));
 % end
-
-
 
 
 function age_Callback(hObject, eventdata, handles)
@@ -312,3 +334,213 @@ function age_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
+
+% --- Executes on button press in stop_buttom.
+function stop_buttom_Callback(hObject, eventdata, handles)
+% hObject    handle to stop_buttom (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+   set(handles.pulse_value, 'String', '');
+   set(handles.max_pulse_value, 'String', '');
+   set(handles.oxy_value, 'String', '');
+   cla(handles.oxy_graph,'reset')
+   cla(handles.waveform_graph,'reset')
+   guidata(hObject,handles);
+  
+% --- Executes on button press in record.
+function record_Callback(hObject, eventdata, handles)
+% hObject    handle to record (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+axes(handles.waveform_graph)
+delete(instrfindall);
+s = serial('COM4');
+
+set(s, 'InputBufferSize', 256);
+set(s, 'BaudRate', 115200);
+fopen(s);
+
+packet = zeros(1, 4);
+pCnt = 1;
+state = 1;
+cnt = 0;
+len = 0;
+sCnt=1;
+j=1;
+tempLrc=0;
+s1Cnt=1;
+bCnt=0;
+byteCnt=1;
+plotCnt=1;
+
+redSig=[];
+irSig=[];
+sig_red=[];
+sig_ir=[];
+t=[];
+
+axes(handles.waveform_graph) 
+handles.waveform_graph=plot(t,sig_red);
+
+for i = 1:10000
+    
+    if s.BytesAvailable > 0
+       byte_array=fread(s, s.BytesAvailable, 'uchar'); 
+       bCnt=length(byte_array);
+       byteCnt=1;
+    end
+    
+    while bCnt >0              
+        byte = byte_array(byteCnt);
+        byteCnt=byteCnt+1;
+        bCnt=bCnt-1;
+        
+        switch state
+            % Preable
+            case 1
+                if(byte == 165)   %hex: 0xA5
+                    packet(pCnt) = byte;
+                    pCnt=pCnt+1;
+                    state = 2;
+                end
+                % Cmd + Param
+            case 2
+                cnt=cnt+1;
+                packet(pCnt) = byte;
+                pCnt=pCnt+1;
+                if(cnt == 2)
+                    state = 3;
+                    cnt = 0;
+                end
+                % Len
+            case 3
+                packet(pCnt) = byte;
+                pCnt=1;
+                len = byte;
+                state = 4;
+                % LRC
+            case 4
+                x = mod(sum(packet), 256) ;
+                lrc=bitcmp(x, 'uint8')+1;
+                   
+                if(byte == lrc)
+                    state = 5;
+                    lrc=0;
+                else
+                    state = 1;
+                end
+            case 5
+                
+                    tempLrc = tempLrc + byte;
+                    
+                    if sCnt==1
+                        sample(j)=byte; %*256;
+                        sCnt=sCnt+1;
+                    elseif sCnt==2
+                        sample(j)=sample(j)+byte*256;
+                        sCnt=1;
+                        j=j+1;
+                    end
+                    
+                    s1Cnt=s1Cnt+1;
+                
+                    if s1Cnt==len+1  
+               
+                        state=6;
+                        s1Cnt=1;
+                        j=1;
+                    else
+                        state=5;
+                    end
+                                   
+            case 6
+                x2 = mod(tempLrc, 256) ;
+                tempLrc=0;
+                lrc2=mod((bitcmp(x2, 'uint8')+1), 256);
+                
+                if(byte == lrc2)
+                    state = 7;
+                    lrc2=0;
+                else
+                    state = 1;
+                end
+                
+            case 7
+                             
+                red=sample(1:2:end);
+                ir=sample(2:2:end);
+                
+%                 red=filter_PPG(red);
+%                 ir=filter_PPG(ir);
+
+                
+                redSig=[redSig red];
+                irSig =[irSig ir];
+                
+                
+                if mod(plotCnt, 20)==0
+                Fs=500;
+                filt=fir1(34, [0.5 20]/[Fs/2]);
+                y_red=filtfilt(filt,1,redSig(end-length(red)*20+1:end));
+                y_red=2^16-y_red;
+                sig_red = [sig_red y_red];
+                
+                y_ir=filtfilt(filt,1,irSig(end-length(ir)*20+1:end));
+                sig_ir = [sig_ir y_ir];
+                    t=1:length(redSig);
+
+%                     [redSig, irSig]=function plot(self)
+%                      plot(t, redSig, t, irSig)
+                      plot(t, sig_red)%, t, sig_ir)
+                 %     set(handles.waveform_graph,'XData',t(end-length(red)*5+1:end),'YData',sig_red((end-length(red)*5+1:end)));
+  
+%                 set(handles.waveform_graph,'XData',t(t > t(end-length(red)*20+1)),'YData',sig_red(t > t(end-length(red)*20+1)));
+ %                axis([t(end-length(red)*20+1) t(end) 0 10000])  
+                 % end
+                end
+                
+                pause(0.0001)
+                plotCnt=plotCnt+1;
+                               
+                state=1;
+                                  
+            otherwise
+                disp('done')
+        end
+        
+    end
+end
+save sig_red.mat sig_red redSig;
+
+fclose(s);
+delete(s);
+clear s;
+
+
+% --- Executes on button press in Forward.
+function Forward_Callback(hObject, eventdata, handles)
+% hObject    handle to Forward (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+         Ax = findall(0,'type','axes');
+         xl=xlim(Ax(4));
+         if xl(2)<(length(handles.filtered_sig)/handles.Fs)
+         axis(Ax(4),[xl(1)+1 xl(2)+1 0 4.5e-3])
+         end
+
+% --- Executes on button press in backward.
+function backward_Callback(hObject, eventdata, handles)
+% hObject    handle to backward (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+Ax = findall(0,'type','axes');
+         xl=xlim(Ax(4));
+         if xl>0
+         axis(Ax(4),[xl(1)-1 xl(2)-1 0 4.5e-3])
+         end
+         
+         
+         
+         
