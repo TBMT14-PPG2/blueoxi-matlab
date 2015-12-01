112b3d1a41110935e90ed4a5137add4d032147bd
filgu983 <filgu983@student.liu.se>
2015-12-01 16:03:31 +0100

    GUI update

--- GUI/GUI.m
+++ GUI/GUI.m
@@ -22,7 +22,7 @@ function varargout = GUI(varargin)
 
 % Edit the above text to modify the response to help GUI
 
-% Last Modified by GUIDE v2.5 01-Dec-2015 13:45:38
+% Last Modified by GUIDE v2.5 01-Dec-2015 15:11:55
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -72,7 +72,6 @@ imshow(g);
 % jToolbar = get(get(hToolbar,'JavaContainer'),'ComponentPeer');
 % jToolbar.setPreferredSize(java.awt.Dimension(10,50));
 % jToolbar.revalidate; % refresh/update the displayed toolbar
-
 
 % --- Outputs from this function are returned to the command line.
 function varargout = GUI_OutputFcn(hObject, eventdata, handles) 
@@ -159,51 +158,23 @@ set(handles.max_pulse_value, 'String', mean(handles.max_pulse));
 set(handles.text21, 'String', 'Average SpO2:');
 set(handles.oxy_value, 'String', mean(handles.saturation));
 
-%Show BPM and O2 in axes2 with tag oxy_graph
+%Show pulse and O2 in axes2 with tag oxy_graph
 axes(handles.oxy_graph)
 [ax,p1,p2] = plotyy(handles.time_peak,handles.pulse,handles.time_peak,handles.saturation,'semilogy','plot');
 xlabel(ax(1),'Time [s]') % label x-axis
 ylabel(ax(1),'BPM') % label left y-axis
 ylabel(ax(2),'% O2') % label right y-axis
-%     ylim([30 200])
-%     ylim(ax(2),[60 110]);
-%     line(le_s,S,'Color','b')
-%     ax(2).XColor = 'b';
-%     ax(2).YColor = 'b';
 line(handles.time_peak,handles.pulse,'Color','k')
 ax(1).XColor = 'k';
 ax(1).YColor = 'k';
-
-%     %Show BPM and O2 in axes2 with tag oxy_graph
-%     axes(handles.oxy_graph)
-%     le=1:length(avg_pulse);
-%     le_ss=1:length(S);
-%     le_s=le_ss/handles.Fs;
-%     [ax,p1,p2] = plotyy(ptp_time,avg_pulse,le_s,S,'semilogy','plot');
-%     xlabel(ax(1),'Time [s]') % label x-axis
-%     ylabel(ax(1),'BPM') % label left y-axis
-%     ylabel(ax(2),'% O2') % label right y-axis
-% %     ylim([30 200])
-% %     ylim(ax(2),[60 110]);
-% %     line(le_s,S,'Color','b')
-% %     ax(2).XColor = 'b';
-% %     ax(2).YColor = 'b';
-%     line(ptp_time,avg_pulse,'Color','k')
-%     ax(1).XColor = 'k';
-%     ax(1).YColor = 'k';
-
-% pause(1/(handles.Fs/10) - toc(timer));
-% end
-
+% line(handles.time_peak,handles.saturation,'Color','r')
+% ax(2).XColor = 'r';
+% ax(2).YColor = 'r';
 
 function age_Callback(hObject, eventdata, handles)
 % hObject    handle to age (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
-% Hints: get(hObject,'String') returns contents of age as text
-%        str2double(get(hObject,'String')) returns contents of age as a double
-
 
 % --- Executes during object creation, after setting all properties.
 function age_CreateFcn(hObject, eventdata, handles)
@@ -211,8 +182,7 @@ function age_CreateFcn(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    empty - handles not created until after all CreateFcns called
 
-% Hint: edit controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
+% Sets the condition for the age window
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
 end
@@ -222,33 +192,26 @@ function stop_buttom_Callback(hObject, eventdata, handles)
 % hObject    handle to stop_buttom (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-
-%    set(handles.pulse_value, 'String', '');
-%    set(handles.max_pulse_value, 'String', '');
-%    set(handles.oxy_value, 'String', '');
-%    cla(handles.oxy_graph,'reset')
-%    cla(handles.waveform_graph,'reset')
-%    guidata(hObject,handles);
   
 % --- Executes on button press in record.
 function record_Callback(hObject, eventdata, handles)
 % hObject    handle to record (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
- axes(handles.waveform_graph)
-    delete(instrfindall);
-    clear s;
-    instrreset;
-    s = tcpip('0.0.0.0', 2222, 'NetworkRole', 'server');
+axes(handles.waveform_graph)
+delete(instrfindall);
+clear s;
+instrreset;
+s = tcpip('0.0.0.0', 2222, 'NetworkRole', 'server');
 
-    set(s, 'InputBufferSize', 256);
-    set(s, 'BaudRate', 115200);
-    fopen(s);     
-    
-    % Checks if the entered film length is a number
+set(s, 'InputBufferSize', 256);
+set(s, 'BaudRate', 115200);
+fopen(s);
+
+% Checks if the entered film length is a number
 if isnan(str2double(get(handles.age,'String'))) == 1
     msgbox('Age must be a number')
-   return
+    return
 else
     age =(str2double(get(handles.age, 'String')));
     % Checks if the entered number is between 0 and 100 years
@@ -258,58 +221,61 @@ else
     end
 end
 
-    packet = zeros(1, 4);
-    pCnt = 1;
-    state = 1;
-    cnt = 0;
-    len = 0;
-    sCnt=1;
-    j=1;
-    tempLrc=0;
-    s1Cnt=1;
-    bCnt=0;
-    byteCnt=1;
-    plotCnt=1;
+% Declared variables
+packet = zeros(1, 4);
+pCnt = 1;
+state = 1;
+cnt = 0;
+len = 0;
+sCnt=1;
+j=1;
+tempLrc=0;
+s1Cnt=1;
+bCnt=0;
+byteCnt=1;
+plotCnt=1;
 
-    redSig=[];
-    irSig=[];
-    sig_red=[];
-    sig_ir=[];
-    k=150;
-    Fs=500;
-    
-    MaxPulse=220-age;
-    PercentageOfMax=round(1000*60/MaxPulse)/10;
-    five_pulses=60*ones(1,5);
-    Pulse=60;
-    lastpeaktime=0;
-    t=[0:1/500:35/500];
-    filt=fir1(34, [0.5 10]/(Fs/2));
-    redSig=zeros(1,35);
-    filtered=t;
-    count=0;
-    ptp_time=0;
-    AC_red=0;
-    DC_red=0;
-    ACfull=0;
-    DCfull=0;
-    AC_IR=0;
-    DC_IR=0;
-    R=1;
-    newR=1;
+redSig=[];
+irSig=[];
+sig_red=[];
+sig_ir=[];
+k=150;
+Fs=500;
 
-    axes(handles.waveform_graph) 
-    handles.waveform_graph=plot(t,filtered,'-k','LineWidth',1.5);
+MaxPulse=220-age;
+PercentageOfMax=round(1000*60/MaxPulse)/10;
+five_pulses=60*ones(1,5);
+Pulse=60;
+lastpeaktime=0;
+t=[0:1/500:35/500];
+filt=fir1(34, [0.5 10]/(Fs/2));
+redSig=zeros(1,35);
+filtered=t;
+count=0;
+ptp_time=0;
+AC_red=0;
+DC_red=0;
+ACfull=0;
+DCfull=0;
+AC_IR=0;
+DC_IR=0;
+R=1;
+newR=1;
 
+axes(handles.waveform_graph)
+handles.waveform_graph=plot(t,filtered,'-k','LineWidth',1.5);
+
+% Gets the data from the device into matlab while stopped button is not
+% pressed.
 while get(handles.stop,'Value') == 0
     
     if s.BytesAvailable > 0
-       byte_array=fread(s, s.BytesAvailable, 'uchar'); 
-       bCnt=length(byte_array);
-       byteCnt=1;
+        byte_array=fread(s, s.BytesAvailable, 'uchar');
+        bCnt=length(byte_array);
+        byteCnt=1;
     end
     
-    while bCnt >0              
+    while bCnt >0
         byte = byte_array(byteCnt);
         byteCnt=byteCnt+1;
         bCnt=bCnt-1;
@@ -341,7 +307,7 @@ while get(handles.stop,'Value') == 0
             case 4
                 x = mod(sum(packet), 256) ;
                 lrc=bitcmp(x, 'uint8')+1;
-                   
+                
                 if(byte == lrc)
                     state = 5;
                     lrc=0;
@@ -350,28 +316,28 @@ while get(handles.stop,'Value') == 0
                 end
             case 5
                 
-                    tempLrc = tempLrc + byte;
-                    
-                    if sCnt==1
-                        sample(j)=byte; %*256;
-                        sCnt=sCnt+1;
-                    elseif sCnt==2
-                        sample(j)=sample(j)+byte*256;
-                        sCnt=1;
-                        j=j+1;
-                    end
-                    
-                    s1Cnt=s1Cnt+1;
+                tempLrc = tempLrc + byte;
                 
-                    if s1Cnt==len+1  
-               
-                        state=6;
-                        s1Cnt=1;
-                        j=1;
-                    else
-                        state=5;
-                    end
-                                   
+                if sCnt==1
+                    sample(j)=byte; %*256;
+                    sCnt=sCnt+1;
+                elseif sCnt==2
+                    sample(j)=sample(j)+byte*256;
+                    sCnt=1;
+                    j=j+1;
+                end
+                
+                s1Cnt=s1Cnt+1;
+                
+                if s1Cnt==len+1
+                    
+                    state=6;
+                    s1Cnt=1;
+                    j=1;
+                else
+                    state=5;
+                end
+                
             case 6
                 x2 = mod(tempLrc, 256) ;
                 tempLrc=0;
@@ -385,93 +351,91 @@ while get(handles.stop,'Value') == 0
                 end
                 
             case 7
-                             
+                
                 red=sample(1:2:end);
                 ir=sample(2:2:end);
                 redSig=[redSig red];
                 irSig =[irSig ir];
                 
-               if mod(plotCnt, 1)==0
-               
-                for i=length(redSig)-length(red)-length(filt)+1:length(redSig)-length(filt)
-                    filtered(i)=2^16-sum(redSig(i:i+length(filt)-1).*filt);
-                    %filteredIR(i)=sum(irSig(i:i+length(filt)-1).*filt);
-                    t(i)=i/500;
-                end
-                count=round(t(end)*500);
-            
-                save Data filtered AC_red DC_red AC_IR DC_IR redSig irSig R ptp_time t
-
-                     if t(end)>4
-                     
-                     set(handles.waveform_graph,'XData',t(t > t(end-2000+1)),'YData',filtered(t>t(end-2000+1)))
-                     axis([t(end-2000+1) t(end) min(filtered(end-2000:end))-2 max(filtered(end-2000:end))+2])
-             
-                     for i=count-length(red)-k:count-k
-                         
-                       if t(i-k)>lastpeaktime+30/Pulse(end)
-                             
-                        if ( ( filtered(i-k)>=max(filtered(i-k+1:i)) )  && ...
-                           (  filtered(i-k)>=max(filtered(i-2*k:i-k-1)) ) )
+                if mod(plotCnt, 1)==0
+                    
+                    for i=length(redSig)-length(red)-length(filt)+1:length(redSig)-length(filt)
+                        filtered(i)=2^16-sum(redSig(i:i+length(filt)-1).*filt);
+                        %filteredIR(i)=sum(irSig(i:i+length(filt)-1).*filt);
+                        t(i)=i/500;
+                    end
+                    count=round(t(end)*500);
+                    
+                    save Data filtered AC_red DC_red AC_IR DC_IR redSig irSig R ptp_time t
+                    
+                    if t(end)>4
+                        
+                        set(handles.waveform_graph,'XData',t(t > t(end-2000+1)),'YData',filtered(t>t(end-2000+1)))
+                        axis([t(end-2000+1) t(end) min(filtered(end-2000:end))-2 max(filtered(end-2000:end))+2])
+                        
+                        for i=count-length(red)-k:count-k
                             
-                            hold on
-                            scatter(t(i-k),filtered(i-k),'or','fill')
-                            lastpeaktime=t(i-k);
-                            ptp_time = [ptp_time lastpeaktime];
-                            five_pulses=[five_pulses(2:5) 60/(ptp_time(end)-ptp_time(end-1))];
-                            Pulse=[Pulse round(mean(five_pulses))];
-                            PercentageOfMax=[PercentageOfMax round(100*Pulse(end)/MaxPulse)]; 
-                            AC_red=[AC_red max(redSig(i-500:end-100))];
-                            DC_red=[DC_red min(redSig(i-500:end-100))];
-                            AC_IR=[AC_IR max(irSig(i-500:end-100))];
-                            DC_IR=[DC_IR min(irSig(i-500:end-100))];
-                            newR=((AC_red(end)-DC_red(end))/DC_red(end))/((AC_IR(end)-DC_IR(end))/DC_IR(end));
-                            R=[R 0.8*R(end)+0.2*newR];
-                            set(handles.pulse_value, 'String', Pulse(end));
-                            set(handles.max_pulse_value, 'String', PercentageOfMax(end));
-                            %plot(handles.oxy_graph,ptp_time,Pulse,'-k',ptp_time,PercentageOfMax,'-r',ptp_time,R,'-b','LineWidth',1.5);
-                            hAx=plotyy(handöes.oxy_graph,ptp_time,Pulse,'-k',tpt_time,Sat,'-r')
-                            ylabel(hAx(1),'Pulse [BPM]')
-                            ylabel(hAx(2),'Saturation [%]')
+                            if t(i-k)>lastpeaktime+30/Pulse(end)
+                                
+                                if ( ( filtered(i-k)>=max(filtered(i-k+1:i)) )  && ...
+                                        (  filtered(i-k)>=max(filtered(i-2*k:i-k-1)) ) )
+                                    
+                                    hold on
+                                    scatter(t(i-k),filtered(i-k),'or','fill')
+                                    lastpeaktime=t(i-k);
+                                    ptp_time = [ptp_time lastpeaktime];
+                                    five_pulses=[five_pulses(2:5) 60/(ptp_time(end)-ptp_time(end-1))];
+                                    Pulse=[Pulse round(mean(five_pulses))];
+                                    PercentageOfMax=[PercentageOfMax round(100*Pulse(end)/MaxPulse)];
+                                    AC_red=[AC_red max(redSig(i-500:end-100))];
+                                    DC_red=[DC_red min(redSig(i-500:end-100))];
+                                    AC_IR=[AC_IR max(irSig(i-500:end-100))];
+                                    DC_IR=[DC_IR min(irSig(i-500:end-100))];
+                                    newR=((AC_red(end)-DC_red(end))/DC_red(end))/((AC_IR(end)-DC_IR(end))/DC_IR(end));
+                                    R=[R 0.8*R(end)+0.2*newR];
+                                    set(handles.pulse_value, 'String', Pulse(end));
+                                    set(handles.max_pulse_value, 'String', PercentageOfMax(end));
+                                    %plot(handles.oxy_graph,ptp_time,Pulse,'-k',ptp_time,PercentageOfMax,'-r',ptp_time,R,'-b','LineWidth',1.5);
+                                    hAx=plotyy(handles.oxy_graph,ptp_time,Pulse,'-k',tpt_time,Sat,'-r')
+                                    ylabel(hAx(1),'Pulse [BPM]')
+                                    ylabel(hAx(2),'Saturation [%]')
+                                end
+                            end
                         end
-                      end
-                     end
-                  end
-               end
+                    end
+                end
                 pause(0.0001)
                 plotCnt=plotCnt+1;
                 state=1;
-                                  
+                
             otherwise
                 disp('done')
         end
     end
 end
 
-button = questdlg('qstring','Do you like to save?','Yes','No',default);
-
 %Construct a questdlg with two options
-choice = questdlg('Do you like to save?', 'Save dialog', 'Yes','No');
+choice = questdlg('Do you like to save?', 'Save dialog');
 
 switch choice
     case 'Yes'
-        uisave({'filtered', 'ptp_time', 'Pulse', 'PercentageOfMax', 'R','t'});   
-    case 'No'
-     
+        uisave({'filtered', 'ptp_time', 'Pulse', 'PercentageOfMax', 'R','t'});
+    case 'No'  
+    case 'Cancel'
 end
 
-    %Display the value of the current pulse in tag pulse_value
-    set(handles.text20, 'String', 'Avg pulse bpm:');
-    set(handles.pulse_value, 'String', mean(Pulse));
-    
-    %Display the value of the maximum pulse in tag max_pulse_value
-    set(handles.text19, 'String', 'Avg percentage max pulse:');
-    set(handles.max_pulse_value, 'String', mean(PercentageOfMax));
-    
-    %Display the value of the current oxy_sat in tag oxy_value
-    % S = sprintf('%d', 98);
-    set(handles.text21, 'String', 'Average SpO2:');
-    set(handles.oxy_value, 'String', mean(R)); % Change to SpO2!!!
+%Display the value of the current pulse in tag pulse_value
+set(handles.text20, 'String', 'Avg pulse bpm:');
+set(handles.pulse_value, 'String', mean(Pulse));
+
+%Display the value of the maximum pulse in tag max_pulse_value
+set(handles.text19, 'String', 'Avg percentage max pulse:');
+set(handles.max_pulse_value, 'String', mean(PercentageOfMax));
+
+%Display the value of the current oxy_sat in tag oxy_value
+% S = sprintf('%d', 98);
+set(handles.text21, 'String', 'Average SpO2:');
+set(handles.oxy_value, 'String', mean(R)); % Change to SpO2!!!
 
 
 fclose(s);
@@ -483,13 +447,15 @@ function Forward_Callback(hObject, eventdata, handles)
 % hObject    handle to Forward (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-         Ax = findall(0,'type','axes');
-         xl=xlim(Ax(4));
-         min_sig = min(handles.signal((xl(1)*500+1):(xl(2)*500)));
-         max_sig = max(handles.signal((xl(1)*500+1):(xl(2)*500)));
-         if xl(2)<(length(handles.signal)/500) % Fs = 500!
-         axis(Ax(4),[xl(1)+1 xl(2)+1 min_sig max_sig])
-         end
+
+% The forward button makes it possible to step forward in the signal
+Ax = findall(0,'type','axes');
+xl=xlim(Ax(4));
+min_sig = min(handles.signal((xl(1)*500+1):(xl(2)*500)));
+max_sig = max(handles.signal((xl(1)*500+1):(xl(2)*500)));
+if xl(2)<(length(handles.signal)/500) % Fs = 500!
+    axis(Ax(4),[xl(1)+1 xl(2)+1 min_sig max_sig])
+end
 
 % --- Executes on button press in backward.
 function backward_Callback(hObject, eventdata, handles)
@@ -497,11 +463,27 @@ function backward_Callback(hObject, eventdata, handles)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
+% The backward button makes it possible to step backward in the signal
 Ax = findall(0,'type','axes');
-         xl=xlim(Ax(4));
-         min_sig = min(handles.signal((xl(1)*500+1):(xl(2)*500)));
-         max_sig = max(handles.signal((xl(1)*500+1):(xl(2)*500)));
-         if xl>0
-         axis(Ax(4),[xl(1)-1 xl(2)-1 min_sig max_sig])
-         end
-        
+xl=xlim(Ax(4));
+min_sig = min(handles.signal((xl(1)*500+1):(xl(2)*500)));
+max_sig = max(handles.signal((xl(1)*500+1):(xl(2)*500)));
+if xl>0
+    axis(Ax(4),[xl(1)-1 xl(2)-1 min_sig max_sig])
+end
+
+% --------------------------------------------------------------------
+function exit_botton_Callback(hObject, eventdata, handles)
+% hObject    handle to exit_botton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+clc
+close all
+
+% --------------------------------------------------------------------
+function help_botton_Callback(hObject, eventdata, handles)
+% hObject    handle to help_botton (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+winopen('hej.pdf')
+
